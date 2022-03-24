39310a4ca68f30492d74ae4a8cc578f0dc5b46a4
Adam Shusterman <shustea1@dsigpu02.local>
2022-03-24 15:54:57 +0200

    Create Mixed data for training samples

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,77 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+from ast import operator
+#from asyncio.windows_events import NULL
+import librosa
+import numpy as np
+import numpy.random as random
+import soundfile as sf
+import os
+fs = 16000
+
+def convert_wv12wav(original_data_dir, wav_data_path, fs):
+    # input - original path, intended wav path, sampling rate
+    # output - void
+    # function converts between WV1 file to in our original path to a WAV file in our intended path, file SR is fs 
+
+    speakers_dirs = os.listdir(original_data_dir)
+
+    for speaker in speakers_dirs:
+        if not os.path.isdir(wav_data_path + speaker): 
+            os.mkdir(wav_data_path + speaker)
+        files = os.listdir(original_data_dir + '/' + speaker)
+        files = [file for file in files if file.split('.')[-1] == 'wv1']
+        for signal in files:
+            wv1_signal, fs = librosa.load(original_data_dir + '/' + speaker + '/' +signal, fs)
+            sf.write(wav_data_path + speaker + '/' + signal.split('.')[0] + '.wav', wv1_signal, fs)
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+def mix_signal(wav_data_path, p_silence, p_one):
+    # input - wav files path, first speaker porbabilty, second speaker probabilty
+    # output - mixed signal of speakers according to their defined probabilty
+    # function uses given probabilty for each speaker to speak, mixes them to 5 sec intervals, and returns (meant to be used twice for our data)
+    fs = 16000
+    five_sec = 5*fs
+    first_speaker = np.random.normal(0,1e-5,five_sec) 
+    second_speaker = np.zeros(five_sec)
+    speakers_dirs = os.listdir(wav_data_path)
+    prob = np.random.rand()
+    if prob > p_silence :
+        first_speaker_id = random.choice(speakers_dirs)
+        first_speaker, fs = librosa.load(wav_data_path + first_speaker_id + '/' + random.choice(os.listdir(wav_data_path + first_speaker_id)), fs)
+        if len(first_speaker) > five_sec :
+            rand_start = random.randint(0,len(first_speaker)-five_sec-1) 
+            first_speaker = first_speaker[rand_start:rand_start+five_sec]
+        else:
+            first_speaker = np.concatenate((first_speaker,np.random.normal(0,1e-5,five_sec-len(first_speaker))))
+        speakers_dirs.remove(first_speaker_id)
+    else:
+        first_speaker_id = 'no_first'
+    if prob > p_one + p_silence: # probabilty for 2 speakers
+        second_speaker_id = random.choice(speakers_dirs)
+        second_speaker, fs = librosa.load(wav_data_path + second_speaker_id + '/' + random.choice(os.listdir(wav_data_path + second_speaker_id)), fs)
+        if len(second_speaker) > five_sec :
+            rand_start = random.randint(0,len(second_speaker)-five_sec-1) #clips bigger then 10 sec
+            second_speaker = second_speaker[rand_start:rand_start+five_sec]
+        else:
+            second_speaker = np.concatenate((second_speaker,np.random.normal(0,1e-5,five_sec-len(second_speaker))))
+        speakers_dirs.remove(second_speaker_id)
+    else:
+        second_speaker_id = 'no_second'
+    mixed_signal = first_speaker + second_speaker
+    return mixed_signal
 
+def CreateSample(wav_data_path, processed_path):
+    fs = 16000
+    for sample in range(5):
+        mix1 = mix_signal(wav_data_path,0,0.5)
+        mix2 = mix_signal(wav_data_path,0.55, 0.15)
+        final_sample = np.concatenate((mix1, mix2))
+        sf.write(processed_path + 'sample_' + str(sample) + '.wav' , final_sample, fs)
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+if __name__ == "__main__":
+    wav_data_path = '/mnt/dsi_vol1/users/shustea1/Data/lstm-speech/data/raw/WSJ0/'
+    original_data_dir = '/mnt/dsi_vol1/users/shustea1/data/sd_et_20'
+    processed_path = '/mnt/dsi_vol1/users/shustea1/Data/lstm-speech/data/processed/WSJ0/'
+    fs = 16000
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+   # convert_wv12wav(original_data_dir, wav_data_path, fs)
+    CreateSample(wav_data_path,processed_path)
